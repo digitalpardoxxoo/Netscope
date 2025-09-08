a916ca5fb3c08e8e457bdc2fd4d9c7aee06550c5
digitalparadoxxoo <mahajanraghav537@gmail.com>
2025-09-08 18:30:11 +0530

    Added Common ports banner grabbing with a ssl wrapping of socket for ssl/tls handkshake allowing for banner grabbing

--- Web_recon/NetRecon/net_scan.py
+++ Web_recon/NetRecon/net_scan.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import ssl
 
 port= input("Enter the Ports or Port you wanna scan : ")
 port=port.split(",")
@@ -15,24 +16,69 @@ def scan(p):
             s.connect((host,p))
             print(f"Port {p} is open ")
             try:
-                 s.send(b"GET / HTTP/1.1\r\nHost: " + host.encode() + b"\r\n\r\n")
-                 banner=s.recv(2048).decode(errors="ignore")
-                 if banner.strip():
-                    print(f"Banner from port {p}:\n{banner}")
+                 #banner Grabbing and port status checking for common ports 
+                 if p==80:
+                    req=(
+                        'GET / HTTP/1.1\r\n'
+                        f'Host:{host}\r\n'
+                        'User-Agent: SimpleScanner/1.0\r\n'
+                        'Connection: close\r\n'
+                        '\r\n'
+                    )
+                    s.send(req.encode())
+                    banner=s.recv(2048).decode(errors="ignore")
+                    print(f"Banner from port {p}:\n{banner.strip()}")
+                 elif p==443:
+                     
+                     s.close()
+                     
+                     context=ssl.create_default_context()
+                     #making a separate socket with ssl wrapping for a possible ssl/tls handshake to get a banner 
+                     ssl_sock=context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM),server_hostname=host)
+                     ssl_sock.connect((host,p))
+                     print(f"Port {p} is open (HTTPS)")
+
+                     req=(
+                        'GET / HTTP/1.1\r\n'
+                        f'Host:{host}\r\n'
+                        'User-Agent: SimpleScanner/1.0\r\n'
+                        'Connection: close\r\n'
+                        '\r\n'
+                        )
+                     ssl_sock.send(req.encode())
+                     banner = ssl_sock.recv(2048).decode(errors="ignore")
+                     print(f"HTTPS Banner from port {p}:\n{banner.strip()}")
+                     ssl_sock.close()
+                    
+
+                 elif p==21:
+                     banner=s.recv(2048).decode(errors="ignore")
+                     print(f" FTP Banner from port {p}:\n{banner.strip()}")
+                 elif p==22:
+                     banner=s.recv(2048).decode(errors="ignore")
+                     print(f"SSH Banner from port {p}:\n{banner.strip()}")
                  else:
-                    print(f"No banner received from port {p}")
+                    s.send(b"\r\n")
+                    banner = s.recv(1024).decode(errors="ignore")
+                    if banner.strip():
+                        print(f"Banner from {p} is:{banner.strip()}")
+                    else:
+                        print(f"No banner received from port {p}")
             except:
                  print(f"Port {p} open, but no banner grabbed")
+
 
         except ConnectionRefusedError:
             print(f"Port {p} is Closed")
         except socket.timeout:
             print(f"Port {p} is filtered")
         except Exception as e:
-            print(f"djdjdj {e}")
+            print(f"Unknow error on Port {p} : {e}")
         finally:
-            s.close()
-
+            try:
+                s.close()
+            except:
+                pass
 #Added threading for Simultaneous port Scans 
 threads=[]
 for p in port:
